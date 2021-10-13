a3f882610ef4e7319f4bef93256fe9fd67218560
Matthew Chen <matthew@signal.org>
2021-10-13 13:19:52 -0300

    Simplify AFNetworking usage.

--- AFNetworking/AFURLRequestSerialization.m
+++ AFNetworking/AFURLRequestSerialization.m
@@ -27,15 +27,137 @@
 #import <CoreServices/CoreServices.h>
 #endif
 
-#import <CocoaLumberjack/CocoaLumberjack.h>
-
-#ifdef DEBUG
-static const NSUInteger ddLogLevel = DDLogLevelAll;
-#else
-static const NSUInteger ddLogLevel = DDLogLevelInfo;
-#endif
-
 NSString * const AFURLRequestSerializationErrorDomain = @"com.alamofire.error.serialization.request";
+
+typedef NSString * (^AFQueryStringSerializationBlock)(NSURLRequest *request, id parameters, NSError *__autoreleasing *error);
+
+/**
+ Returns a percent-escaped string following RFC 3986 for a query string key or value.
+ RFC 3986 states that the following characters are "reserved" characters.
+ - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+ - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+ 
+ In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+ query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+ should be percent-escaped in the query string.
+ - parameter string: The string to be percent-escaped.
+ - returns: The percent-escaped string.
+ */
+NSString * AFPercentEscapedStringFromString(NSString *string) {
+    static NSString * const kAFCharactersGeneralDelimitersToEncode = @":#[]@"; // does not include "?" or "/" due to RFC 3986 - Section 3.4
+    static NSString * const kAFCharactersSubDelimitersToEncode = @"!$&'()*+,;=";
+    
+    NSMutableCharacterSet * allowedCharacterSet = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [allowedCharacterSet removeCharactersInString:[kAFCharactersGeneralDelimitersToEncode stringByAppendingString:kAFCharactersSubDelimitersToEncode]];
+    
+    // FIXME: https://github.com/AFNetworking/AFNetworking/pull/3028
+    // return [string stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+    
+    static NSUInteger const batchSize = 50;
+    
+    NSUInteger index = 0;
+    NSMutableString *escaped = @"".mutableCopy;
+    
+    while (index < string.length) {
+        NSUInteger length = MIN(string.length - index, batchSize);
+        NSRange range = NSMakeRange(index, length);
+        
+        // To avoid breaking up character sequences such as 👴🏻👮🏽
+        range = [string rangeOfComposedCharacterSequencesForRange:range];
+        
+        NSString *substring = [string substringWithRange:range];
+        NSString *encoded = [substring stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+        [escaped appendString:encoded];
+        
+        index += range.length;
+    }
+    
+    return escaped;
+}
+
+#pragma mark -
+
+@interface AFQueryStringPair : NSObject
+@property (readwrite, nonatomic, strong) id field;
+@property (readwrite, nonatomic, strong) id value;
+
+- (instancetype)initWithField:(id)field value:(id)value;
+
+- (NSString *)URLEncodedStringValue;
+@end
+
+@implementation AFQueryStringPair
+
+- (instancetype)initWithField:(id)field value:(id)value {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    self.field = field;
+    self.value = value;
+    
+    return self;
+}
+
+- (NSString *)URLEncodedStringValue {
+    if (!self.value || [self.value isEqual:[NSNull null]]) {
+        return AFPercentEscapedStringFromString([self.field description]);
+    } else {
+        return [NSString stringWithFormat:@"%@=%@", AFPercentEscapedStringFromString([self.field description]), AFPercentEscapedStringFromString([self.value description])];
+    }
+}
+
+@end
+
+#pragma mark -
+
+FOUNDATION_EXPORT NSArray * AFQueryStringPairsFromDictionary(NSDictionary *dictionary);
+FOUNDATION_EXPORT NSArray * AFQueryStringPairsFromKeyAndValue(NSString *key, id value);
+
+NSString * AFQueryStringFromParameters(NSDictionary *parameters) {
+    NSMutableArray *mutablePairs = [NSMutableArray array];
+    for (AFQueryStringPair *pair in AFQueryStringPairsFromDictionary(parameters)) {
+        [mutablePairs addObject:[pair URLEncodedStringValue]];
+    }
+    
+    return [mutablePairs componentsJoinedByString:@"&"];
+}
+
+NSArray * AFQueryStringPairsFromDictionary(NSDictionary *dictionary) {
+    return AFQueryStringPairsFromKeyAndValue(nil, dictionary);
+}
+
+NSArray * AFQueryStringPairsFromKeyAndValue(NSString *key, id value) {
+    NSMutableArray *mutableQueryStringComponents = [NSMutableArray array];
+    
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"description" ascending:YES selector:@selector(compare:)];
+    
+    if ([value isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *dictionary = value;
+        // Sort dictionary keys to ensure consistent ordering in query string, which is important when deserializing potentially ambiguous sequences, such as an array of dictionaries
+        for (id nestedKey in [dictionary.allKeys sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            id nestedValue = dictionary[nestedKey];
+            if (nestedValue) {
+                [mutableQueryStringComponents addObjectsFromArray:AFQueryStringPairsFromKeyAndValue((key ? [NSString stringWithFormat:@"%@[%@]", key, nestedKey] : nestedKey), nestedValue)];
+            }
+        }
+    } else if ([value isKindOfClass:[NSArray class]]) {
+        NSArray *array = value;
+        for (id nestedValue in array) {
+            [mutableQueryStringComponents addObjectsFromArray:AFQueryStringPairsFromKeyAndValue([NSString stringWithFormat:@"%@[]", key], nestedValue)];
+        }
+    } else if ([value isKindOfClass:[NSSet class]]) {
+        NSSet *set = value;
+        for (id obj in [set sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            [mutableQueryStringComponents addObjectsFromArray:AFQueryStringPairsFromKeyAndValue(key, obj)];
+        }
+    } else {
+        [mutableQueryStringComponents addObject:[[AFQueryStringPair alloc] initWithField:key value:value]];
+    }
+    
+    return mutableQueryStringComponents;
+}
 
 #pragma mark -
 
@@ -133,10 +255,6 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
     outputStream.delegate = outputStreamDelegate;
     [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     [outputStream open];
-
-    DDLogInfo(@"outputFileURL: %@", outputFileURL);
-    DDLogInfo(@"outputStream.streamStatus: %lu", (unsigned long) outputStream.streamStatus);
-    [DDLog flushLog];                                                                                              \
     
     if (outputStream.streamStatus != NSStreamStatusOpen) {
         NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedStringFromTable(@"File URL not reachable.", @"AFNetworking", nil)};
@@ -181,7 +299,7 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
 
     closeOutputStream();
 
-    if (outputStreamDelegate.hadError) {
+    if (outputStream.streamStatus != NSStreamStatusClosed || outputStreamDelegate.hadError) {
         if (error) {
             *error = [[NSError alloc] initWithDomain:AFURLRequestSerializationErrorDomain code:NSURLErrorBadURL userInfo:nil];
         }
@@ -210,6 +328,44 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
     NSParameterAssert(name);
     NSParameterAssert(fileName);
     NSParameterAssert(mimeType);
+        
+    NSStringEncoding stringEncoding = self.stringEncoding;
+    
+    NSData *encapsulationBoundaryData = [(hasInitialBoundary
+                                          ? AFMultipartFormInitialBoundary(boundary)
+                                          : AFMultipartFormEncapsulationBoundary(boundary)) dataUsingEncoding:stringEncoding];
+    if (![self writeData:encapsulationBoundaryData outputStream:outputStream error:error]) {
+        return NO;
+    }
+    
+    NSDictionary *headers = [self headersForBodyWithName:name
+                                                fileName:fileName
+                                                mimeType:mimeType];
+    NSString *headersString = [self stringForHeaders:headers];
+    NSData *headersData = [headersString dataUsingEncoding:stringEncoding];
+    if (![self writeData:headersData outputStream:outputStream error:error]) {
+        return NO;
+    }
+    
+    if (![self writeInputFileURL:inputFileURL outputStream:outputStream error:error]) {
+        return NO;
+    }
+    
+    NSData *closingBoundaryData = (hasFinalBoundary
+                                   ? [AFMultipartFormFinalBoundary(boundary) dataUsingEncoding:stringEncoding]
+                                   : [NSData data]);
+    if (![self writeData:closingBoundaryData outputStream:outputStream error:error]) {
+        return NO;
+    }
+    
+    return YES;
+}
+
++ (BOOL)writeInputFileURL:(NSURL *)inputFileURL
+             outputStream:(NSOutputStream *)outputStream
+                    error:(NSError * __autoreleasing *)error
+{
+    NSParameterAssert(inputFileURL);
     
     if (![inputFileURL isFileURL]) {
         NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedStringFromTable(@"Expected URL to be a file URL", @"AFNetworking", nil)};
@@ -230,12 +386,6 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
         return NO;
     }
     
-    NSStringEncoding stringEncoding = self.stringEncoding;
-    
-    NSMutableDictionary *mutableHeaders = [NSMutableDictionary dictionary];
-    [mutableHeaders setValue:[NSString stringWithFormat:@"form-data; name=\"%@\"; filename=\"%@\"", name, fileName] forKey:@"Content-Disposition"];
-    [mutableHeaders setValue:mimeType forKey:@"Content-Type"];
-    
     NSInputStream *inputStream = [NSInputStream inputStreamWithURL:inputFileURL];
     AFStreamDelegate *inputStreamDelegate = [AFStreamDelegate new];
     inputStream.delegate = inputStreamDelegate;
@@ -253,24 +403,6 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
         [inputStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
         [inputStream close];
     };
-
-    NSData *encapsulationBoundaryData = [(hasInitialBoundary
-                                          ? AFMultipartFormInitialBoundary(boundary)
-                                          : AFMultipartFormEncapsulationBoundary(boundary)) dataUsingEncoding:stringEncoding];
-    if (![self writeData:encapsulationBoundaryData outputStream:outputStream error:error]) {
-        closeInputStream();
-        return NO;
-    }
-    
-    NSDictionary *headers = [self headersForBodyWithName:name
-                                                fileName:fileName
-                                                mimeType:mimeType];
-    NSString *headersString = [self stringForHeaders:headers];
-    NSData *headersData = [headersString dataUsingEncoding:stringEncoding];
-    if (![self writeData:headersData outputStream:outputStream error:error]) {
-        closeInputStream();
-        return NO;
-    }
     
     if (![self writeBodyInputStream:inputStream
                        outputStream:outputStream
@@ -279,23 +411,15 @@ static inline NSString * AFMultipartFormFinalBoundary(NSString *boundary) {
         return NO;
     }
     
-    NSData *closingBoundaryData = (hasFinalBoundary
-                                   ? [AFMultipartFormFinalBoundary(boundary) dataUsingEncoding:stringEncoding]
-                                   : [NSData data]);
-    if (![self writeData:closingBoundaryData outputStream:outputStream error:error]) {
-        closeInputStream();
-        return NO;
-    }
-    
     closeInputStream();
-
-    if (inputStreamDelegate.hadError) {
+    
+    if (inputStream.streamStatus != NSStreamStatusClosed || inputStreamDelegate.hadError) {
         if (error) {
             *error = [[NSError alloc] initWithDomain:AFURLRequestSerializationErrorDomain code:NSURLErrorBadURL userInfo:nil];
         }
         return NO;
     }
-
+    
     return YES;
 }
 
